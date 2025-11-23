147e5b7ebd53823fe0a34984089b189bfc82b3c7
FiaAddow <Farhia_dahir_addow@hotmail.no>
2025-11-23 02:17:35 +0100

    header image set

---
+++ app/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="max-w-lg mx-auto mt-20">
+            <img src="../images/header.png" alt="Header Image" className="w-full h-64 object-cover object-center mb-8 rounded-lg" />
+        </div>
+    )
+
+}
